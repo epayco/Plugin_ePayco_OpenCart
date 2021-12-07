097921db68e7d2518fd316fbd760917e143f3052
RicardoSaldarriagaPayco <ricardo.saldarriaga@payco.co>
2021-12-06 19:09:40 -0500

    feat: se restructura el codigo y se realiza actualziacion de version

--- opencart-2-2/catalog/language/spanish/payment/payco.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Text
-$_['text_title'] = 'Reciba pagos con tarjeta de crédito, débito o efectivo con (ePayco.com)';
-$_['alert_comision'] = 'Importante. Se cobrará una comisión de %comision% sobre el total de la compra.';
-?>
